fa6c731a8d2ae931821feb9af5e222c8c03d6080
VivekKori <vivekkori58@gmail.com>
2025-04-20 12:14:13 +0530

    Navbar created using bootstrap

--- paste-app/src/components/Navbar.jsx
+++ paste-app/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import { NavLink } from "react-router";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Paste App
+          </a>
+          
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav d-flex link-group ">
+              <NavLink className="nav-link nav-links " aria-current="page" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-link nav-links " aria-current="page" to="/pastes">
+                Pastes
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;

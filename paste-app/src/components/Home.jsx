0387d65684135313057dac47de64460e8bf335a9
VivekKori <vivekkori58@gmail.com>
2025-04-21 15:04:06 +0530

    just add function without defnation in slice

--- paste-app/src/components/Home.jsx
+++ paste-app/src/components/Home.jsx
@@ -1,30 +1,61 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import {useDispatch} from 'react-redux'
 
 const Home = () => {
   const [title, setTitle] = useState("");
   const [value, setValue] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const pasteId = searchParams.get("pasteId");
+  const dispatch = useDispatch();
+
+  const CreatePaste = () => {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId || Date.now().toString(36),
+      createdAt: new Date().toISOString(),
+    }
+    if(pasteId){
+      //update
+      dispatch(updateToPaste(paste));
+    }else {
+      //create
+      dispatch(addToPaste(paste));
+    }
+
+    //after updattion or creation
+
+    setTitle('')
+    setValue('')
+    setSearchParams({})
+  };
 
   return (
-   <center>
-     <div>
-      <input
-        className="p-2 m-4 rounded-3 border border-secondary-subtle input"
-        type="text"
-        placeholder="Enter Title here..."
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button className="btn p-2 m-4 btn-outline-success button">
-        {pasteId ? "Update My Paste" : "Create My Paste"}
-      </button>
-    </div>
-    <div>
-      <textarea placeholder="Enter Content Here..." rows="20" className="m-4 mt-2 p-2 rounded-3 border border-secondary-subtle text-area"></textarea>
-    </div>
-   </center>
+    <center>
+      <div>
+        <input
+          className="p-2 m-4 rounded-3 border border-secondary-subtle input"
+          type="text"
+          placeholder="Enter Title here..."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          className="btn p-2 m-4 btn-outline-success button"
+          onClick={CreatePaste}
+        >
+          {pasteId ? "Update My Paste" : "Create My Paste"}
+        </button>
+      </div>
+      <div>
+        <textarea
+          placeholder="Enter Content Here..."
+          rows="20"
+          className="m-4 mt-2 p-2 rounded-3 border border-secondary-subtle text-area"
+        ></textarea>
+      </div>
+    </center>
   );
 };
 
